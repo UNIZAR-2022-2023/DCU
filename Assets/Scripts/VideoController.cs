386c2203ef80cf398974699647835377082caa7e
p3n4x0 <Penacho.AE@gmail.com>
2023-04-12 12:17:33 +0200

    null

--- Assets/Scripts/VideoController.cs
+++ Assets/Scripts/VideoController.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class VideoController : MonoBehaviour
 {
+    public VideoPlayer video;
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    void Awake(){
+        video = GetComponent<VideoPlayer>();
+        video.Play();
+        video.loopPointReached += CheckOver;
+    }
+
+    void CheckOver(VideoPlayer  vp){
+        gameObject.SetActive(false);
     }
 
     // Update is called once per frame
